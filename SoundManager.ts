2ba70eb4a1feb0e031b22b49761d54d5002bc8ff
huxiaohei <huliuworld@yahoo.com>
2019-08-30 21:28:06 +0800

    优化 统一api 提供静音接口
    1.音效全部使用cocos creator api统一处理
    2.提供静音接口

--- SoundManager.ts
+++ SoundManager.ts
@@ -10,10 +10,9 @@ import { Utils } from "./Utils";
 export class SoundManager {
     private static readonly _instance: SoundManager = new SoundManager();
     private readonly TAG: string = "SoundManager";
-    private _recyclePool: wx.InnerAudioContext[] = [];
-    private _bgmAudioContext: wx.InnerAudioContext = null;
     private _bgmAudioId: number = -1;
     private _isStopEffect: boolean = false;
+    private _mute: boolean = false;
 
     private constructor() {
         if (SoundManager.getInstance()) {
@@ -25,25 +24,25 @@ export class SoundManager {
         return this._instance;
     }
 
-    public playMusic(url: string, loop?: boolean) {
-        if (!url) {
+    /**
+     * 是否静音
+     */
+    public isMute(): boolean {
+        return this._mute;
+    }
+
+    /**
+     * 播放音乐
+     * @param url 
+     * @param loop 
+     */
+    public playMusic(url: string, loop: boolean = true) {
+        if (!url || this._mute) {
             Utils.LOGE(this.TAG, "music address is invalid");
             return;
         }
-        if (loop === false) {
-            loop = false;
-        } else {
-            loop = true;
-        }
-        if (url.indexOf("http") == 0 && cc.sys.platform == cc.sys.WECHAT_GAME) {
-            if (!this._bgmAudioContext) {
-                this._bgmAudioContext = wx.createInnerAudioContext();
-            }
-            this._bgmAudioContext.stop();
-            this._bgmAudioContext.src = url;
-            this._bgmAudioContext.loop = loop;
-            this._bgmAudioContext.play();
-        } else if (url.indexOf("http") == 0) {
+        this.stopMusic();
+        if (url.indexOf("http") == 0) {
             cc.loader.load(url, (err, clip: cc.AudioClip) => {
                 if (!err) {
                     Utils.LOGE(this.TAG, "play music error : " + url);
@@ -62,30 +61,19 @@ export class SoundManager {
         }
     }
 
+    /**
+     * 播放音效
+     * @param url 
+     */
     public playEffectSound(url) {
-        if (this._isStopEffect) {
+        if (this._isStopEffect || this._mute) {
             return;
         }
         if (!url) {
             Utils.LOGE(this.TAG, "effect sound address is invalid");
             return;
         }
-        if (url.indexOf("http") == 0 && cc.sys.platform == cc.sys.WECHAT_GAME) {
-            var effectContext: wx.InnerAudioContext = null;
-            if (this._recyclePool.length == 0) {
-                effectContext = wx.createInnerAudioContext();
-            } else {
-                effectContext = this._recyclePool.pop();
-            }
-            effectContext.src = url;
-            effectContext.loop = false;
-            effectContext.onStop(() => {
-                effectContext.offStop(() => {
-                    Utils.LOGD(this.TAG, "off stop success");
-                });
-                this._recyclePool.push(effectContext);
-            });
-        } else if (url.indexOf("http") == 0) {
+        if (url.indexOf("http") == 0) {
             cc.loader.load(url, (err: any, clip: cc.AudioClip) => {
                 if (err) {
                     Utils.LOGE(this.TAG, "play effect error : " + url);
@@ -104,35 +92,63 @@ export class SoundManager {
         }
     }
 
+    /**
+     * 暂停音乐
+     */
+    public pauseMusic() {
+        if (this._bgmAudioId >= 0) {
+            cc.audioEngine.pauseMusic();
+        }
+    }
+
+    /**
+     * 停止音乐
+     */
     public stopMusic() {
-        if (cc.sys.platform == cc.sys.WECHAT_GAME) {
-            if (this._bgmAudioContext) {
-                this._bgmAudioContext.stop();
-            }
-        } else {
-            if (this._bgmAudioId > 0) {
-                cc.audioEngine.stopMusic();
-            }
+        if (this._bgmAudioId >= 0) {
+            cc.audioEngine.stopMusic();
+            this._bgmAudioId = -1;
         }
     }
 
+    /**
+     * 恢复音乐
+     */
     public resumeMusic() {
-        if (cc.sys.platform == cc.sys.WECHAT_GAME) {
-            if (this._bgmAudioContext) {
-                this._bgmAudioContext.play();
-            }
-        } else {
-            if (this._bgmAudioId > 0) {
-                cc.audioEngine.resumeMusic();
-            }
+        if (this._bgmAudioId >= 0) {
+            cc.audioEngine.resumeMusic();
         }
     }
 
-    public stopEffect() {
+    /**
+     * 停止音效
+     */
+    public closeEffect() {
         this._isStopEffect = true;
+        cc.audioEngine.stopAllEffects();
     }
 
-    public resumeEffect() {
+    /**
+     * 开启音效
+     */
+    public openEffect() {
         this._isStopEffect = false;
+    }
+
+    /**
+     * 静音
+     */
+    public mute() {
+        this._mute = true;
+        cc.audioEngine.stopAllEffects();
+        this.pauseMusic();
+    }
+
+    /**
+     * 关闭静音
+     */
+    public unmute() {
+        this._mute = false;
+        this.resumeMusic();
     }
 }
